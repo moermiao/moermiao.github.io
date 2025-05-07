2974cb723b7bf731bed4fef5b159b2f47829a058
moermiao <168224984+moermiao@users.noreply.github.com>
2025-05-07 15:04:11 +0300

    Add files via upload

---
+++ fun.js
@@ -0,0 +1,12 @@
+function calcD(a,b,c){
+    return b*b - 4*a*c;
+}
+console.log(calcD(1,2,3))
+
+
+function min(a, b) {
+    return a < b ? a : b;
+}
+console.log(min(5, 3));
+
+document.getElementsByTagId("name").style = "text-decoration:line-through"
